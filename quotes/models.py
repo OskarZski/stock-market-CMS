e65f4f124f3b75e3b66c5d908c32e0f259078cb9
Pete Naveen Pearson <coffee.drinker.daniel@gmail.com>
2020-11-21 05:32:40 -0500

    Add holdings attribute as Decimal

--- quotes/models.py
+++ quotes/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 class Stock(models.Model):
 	ticker = models.CharField(max_length=10)
-
-	def __str__(self):
+	holdings = models.DecimalField(max_digits=10, decimal_places=5)
+	
+ 	def __str__(self):
 		return self.ticker
