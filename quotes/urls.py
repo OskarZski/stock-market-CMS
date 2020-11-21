0ea483bfb24aa830e25be243ff59617e7f201dc9
Pete Naveen Pearson <coffee.drinker.daniel@gmail.com>
2020-11-21 05:34:42 -0500

    Change 'portfolio' to top level landing page route

--- quotes/urls.py
+++ quotes/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('', views.home, name="home"),
-	path('about.html', views.about, name="about"),
-	path('portolio.html', views.portolio, name="portolio"),
+	path('removed', views.home, name="home"),
+	# path('about.html', views.about, name="about"),
+	path('', views.portfolio, name="portfolio"),
 	path('delete/<stock_id>', views.delete, name="delete"),
 	path('delete_stock.html', views.delete_stock, name="delete_stock"),
 
