d05a31fba35b5e67283acc450885a367a1c3f1b5
John Maged <john.maged@swisodev.com>
2020-11-24 19:40:53 +0200

    Fixed routing issue from delete_stock to portfolio

--- quotes/views.py
+++ quotes/views.py
@@ -54,7 +54,7 @@ def delete(request, stock_id):
 	item = Stock.objects.get(pk=stock_id)
 	item.delete()
 	messages.success(request, ("Stock Has Been Deleted!"))
-	return redirect(delete_stock)
+	return redirect(portfolio)
 
 
 
